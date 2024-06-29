5a6f548b204401210f35ec3aa6bc59548fef26c1
gregory1175 <151869985+gregory1175@users.noreply.github.com>
2024-06-30 01:43:13 +0300

    optButtonCom

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,23 +1,31 @@
+import React from 'react';
 import { Text } from 'components/text';
-
 import styles from './Button.module.scss';
 
-export const Button = ({
-	title,
-	onClick,
-	type,
-}: {
+// Добавим типизацию 
+type ButtonProps = {
 	title: string;
 	onClick?: () => void;
 	type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
+	disabled?: boolean;
+};
+
+export const Button: React.FC<ButtonProps> = React.memo(({
+	title,
+	onClick,
+	type,
+	disabled = false,
 }) => {
 	return (
-		<button className={styles.button} 
-		type={type} 
-		onClick={onClick}>
+		<button 
+			className={styles.button} 
+			type={type} 
+			onClick={onClick} 
+			disabled={disabled}
+		>
 			<Text weight={800} uppercase>
 				{title}
 			</Text>
 		</button>
 	);
-};
+});
